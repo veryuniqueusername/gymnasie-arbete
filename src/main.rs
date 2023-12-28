56be876a198f7a142ba16cac0191ef98ffe4abc5
veryuniqueusername <70266105+veryuniqueusername@users.noreply.github.com>
2023-12-28 13:46:27 +0100

    switch to macroquad, also fix the math being incorrect (r_magnitude)

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,139 @@
+use macroquad::prelude::*;
 use nalgebra::Vector3;
-use three_body::run;
 
 const DELTA_TIME: f64 = 0.001;
 const SIMULATION_TIME: f64 = 60.0;
+const FRAMERATE: f64 = 60.0;
+const SCALE: f64 = 200.0;
+const COLORS: [macroquad::color::Color; 3] = [
+    Color::new(1.0, 0.5, 0.5, 1.0),
+    Color::new(0.5, 1.0, 0.5, 1.0),
+    Color::new(0.5, 0.5, 1.0, 1.0),
+];
 
-fn main() {
-    run();
+#[derive(Debug)]
+struct Body {
+    r: Vector3<f64>,
+    v: Vector3<f64>,
+    a: Vector3<f64>,
 }
 
-fn _main() {
-    let mut positions: Vec<(Vector3<f64>, Vector3<f64>, Vector3<f64>)> = Vec::new();
-
-    // Mass
-    const M_A: f64 = 1.0;
-    const M_B: f64 = 1.0;
-    const M_C: f64 = 1.0;
-
-    // Positions
-    let mut r_a: Vector3<f64> = Vector3::new(1.0, 0.0, 0.0);
-    let mut r_b: Vector3<f64> = Vector3::new(-0.5, 0.8660254038, 0.0);
-    let mut r_c: Vector3<f64> = Vector3::new(-0.5, -0.8660254038, 0.0);
-
-    // Momenta
-    let mut p_a: Vector3<f64> = Vector3::new(0.0, 0.5, 0.0);
-    let mut p_b: Vector3<f64> = Vector3::new(-0.4330127019, -0.25, 0.0);
-    let mut p_c: Vector3<f64> = Vector3::new(0.4330127019, -0.25, 0.0);
-
-    // Force vectors, f_ab means the force A has on B; A pulls B with the force f_ab
-    // f_ba = -f_ab
-    let mut f_ab: Vector3<f64>;
-    let mut f_ac: Vector3<f64>;
-    let mut f_bc: Vector3<f64>;
-
-    let mut time = 0.0;
-
-    while time < SIMULATION_TIME {
-        // Calculate the new forces and assign them to their variables
-        f_ab = calculate_gravitational_force(M_A, r_a, M_B, r_b);
-        f_ac = calculate_gravitational_force(M_A, r_a, M_C, r_c);
-        f_bc = calculate_gravitational_force(M_B, r_b, M_C, r_c);
-
-        // Update momenta
-        p_a += ((-f_ab) + -(f_ac)) * DELTA_TIME;
-        p_b += ((f_ab) + -(f_bc)) * DELTA_TIME;
-        p_c += ((f_ac) + (f_bc)) * DELTA_TIME;
-
-        // Update positions
-        r_a += p_a / M_A * DELTA_TIME;
-        r_b += p_b / M_B * DELTA_TIME;
-        r_c += p_c / M_C * DELTA_TIME;
-
-        time += DELTA_TIME;
-
-        positions.push((r_a, r_b, r_c));
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Simulation".to_owned(),
+        window_width: 1200,
+        window_height: 700,
+        ..Default::default()
     }
-
-    println!("{:?}", positions);
 }
 
-// Outputs the force that 1 pulls on 2
-fn calculate_gravitational_force(
-    m_1: f64,
-    r_1: Vector3<f64>,
-    m_2: f64,
-    r_2: Vector3<f64>,
-) -> Vector3<f64> {
-    // F = G * m_1 * m_2 * r_hat / r^2
-    // G = 6.67430 * 10^-11
-    const G: f64 = 0.0000000000667430;
-    let r: Vector3<f64> = r_1 - r_2;
-    let r_magnitude = r.x.powi(2) + r.y.powi(2) + r.z.powi(2);
-    let r_hat: Vector3<f64> = r / r_magnitude;
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut elapsed_time = 0.0;
 
-    let f = G * m_1 * m_2 / r_magnitude.powi(2);
+    let mut bodies = [
+        Body {
+            r: Vector3::new(0.0, 0.0, 0.0),
+            v: Vector3::new(-0.93240737, -0.86473146, 0.0),
+            a: Vector3::zeros(),
+        },
+        Body {
+            r: Vector3::new(-0.97000436, 0.24308753, 0.0),
+            v: Vector3::new(0.4662036850, 0.4323657300, 0.0),
+            a: Vector3::zeros(),
+        },
+        Body {
+            r: Vector3::new(0.97000436, -0.24308753, 0.0),
+            v: Vector3::new(0.4662036850, 0.4323657300, 0.0),
+            a: Vector3::zeros(),
+        },
+    ];
 
-    f * r_hat
+    loop {
+        clear_background(Color::from_hex(0x1a1223)); // Background
+        draw_circle(screen_width() / 2.0, screen_height() / 2.0, 2.0, WHITE); // Center
+        for i in (-(screen_width() / SCALE as f32) as i32 / 2)
+            ..=((screen_width() / SCALE as f32) as i32 / 2)
+        {
+            draw_line(
+                screen_width() / 2.0 + SCALE as f32 * i as f32,
+                0.0,
+                screen_width() / 2.0 + SCALE as f32 * i as f32,
+                screen_height(),
+                0.5,
+                GRAY,
+            )
+        }
+        for i in (-(screen_height() / SCALE as f32) as i32 / 2)
+            ..=((screen_height() / SCALE as f32) as i32 / 2)
+        {
+            draw_line(
+                0.0,
+                screen_height() / 2.0 + SCALE as f32 * i as f32,
+                screen_width(),
+                screen_height() / 2.0 + SCALE as f32 * i as f32,
+                0.5,
+                GRAY,
+            )
+        }
+        draw_text(&get_fps().to_string(), 5.0, 20.0, 20.0, WHITE); // FPS
+        draw_text(&elapsed_time.to_string(), 5.0, 40.0, 20.0, WHITE); // Elapsed time
+
+        let iteration_start_time = get_time();
+        'calculations: loop {
+            for body in bodies.iter_mut() {
+                body.a = Vector3::zeros();
+            }
+
+            for i in 0..bodies.len() {
+                for j in (i + 1)..bodies.len() {
+                    // Calculate gravitational force with direction from i to j
+                    let r: Vector3<f64> = bodies[j].r - bodies[i].r;
+                    let f: Vector3<f64> = r / r.magnitude().powi(3);
+                    bodies[i].a += f;
+                    bodies[j].a -= f;
+                }
+            }
+
+            // Update velocity
+            for body in bodies.iter_mut() {
+                body.v += body.a * DELTA_TIME;
+            }
+
+            // Update position
+            for body in bodies.iter_mut() {
+                body.r += body.v * DELTA_TIME;
+            }
+
+            elapsed_time += DELTA_TIME;
+
+            if get_time() - iteration_start_time > 1.0 / FRAMERATE {
+                break 'calculations;
+            }
+        }
+
+        for i in 0..bodies.len() {
+            let pos_x: f32 = (screen_width() / 2.0) + ((bodies[i].r.x * SCALE) as f32);
+            let pos_y: f32 = (screen_height() / 2.0) - ((bodies[i].r.y * SCALE) as f32);
+            draw_circle(pos_x, pos_y, 5.0, COLORS[i]);
+            draw_line(
+                pos_x,
+                pos_y,
+                pos_x + ((bodies[i].v.x * SCALE) as f32),
+                pos_y - ((bodies[i].v.y * SCALE) as f32),
+                2.0,
+                RED,
+            );
+            draw_line(
+                pos_x,
+                pos_y,
+                pos_x + ((bodies[i].a.x * SCALE) as f32),
+                pos_y - ((bodies[i].a.y * SCALE) as f32),
+                2.0,
+                YELLOW,
+            );
+        }
+
+        next_frame().await
+    }
 }
